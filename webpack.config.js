becc71a68adc514df1a7362bbd4a9c4239a76bc4
jtravi4355 <jrtravi43@gmail.com>
2020-07-27 23:39:15 -0400

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['@babel/env']
+            }
+        }]
+    }
+}
